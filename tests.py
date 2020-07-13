1fe367a9dd2ada358db279080bf75002a74c4c5e
Scott <hudsonsc@oregonstate.edu>
2020-07-13 14:01:22 -0600

    Added new tests to visa.

--- tests.py
+++ tests.py
@@ -14,6 +14,18 @@ class CreditCardTests(unittest.TestCase):
             num = random.randint(40000000000000000, 49999999999999999)
             credit_card_validator(num)
 
+    def visa_16_digits(self):
+        number_of_tests = 100
+        for i in range(number_of_tests):
+            num = random.randint(4000000000000000, 4999999999999999)
+            credit_card_validator(num)
+
+    def visa_15_digits(self):
+        number_of_tests = 100
+        for i in range(number_of_tests):
+            num = random.randint(400000000000000, 499999999999999)
+            credit_card_validator(num)
+
 
 # Master Card generators
 
