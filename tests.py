a3fec3026c6cf216f86ffb57152e936abe403497
Scott <hudsonsc@oregonstate.edu>
2020-07-13 14:02:29 -0600

    Brute forcing the visa thing to see if i can get an error at all.

--- tests.py
+++ tests.py
@@ -9,19 +9,19 @@ from credit_card_validator import credit_card_validator
 class CreditCardTests(unittest.TestCase):
 
     def visa_17_digits(self):
-        number_of_tests = 100
+        number_of_tests = 10000
         for i in range(number_of_tests):
             num = random.randint(40000000000000000, 49999999999999999)
             credit_card_validator(num)
 
     def visa_16_digits(self):
-        number_of_tests = 100
+        number_of_tests = 10000
         for i in range(number_of_tests):
             num = random.randint(4000000000000000, 4999999999999999)
             credit_card_validator(num)
 
     def visa_15_digits(self):
-        number_of_tests = 100
+        number_of_tests = 10000
         for i in range(number_of_tests):
             num = random.randint(400000000000000, 499999999999999)
             credit_card_validator(num)
