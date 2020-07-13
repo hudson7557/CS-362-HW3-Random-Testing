e700cd6f487e5266aafb0bd52885bd0c2debcfc5
Scott <hudsonsc@oregonstate.edu>
2020-07-13 15:23:42 -0600

    Remove uncalled functions lol

--- tests.py
+++ tests.py
@@ -9,27 +9,22 @@ from credit_card_validator import credit_card_validator
 
 class CreditCardTests(unittest.TestCase):
 
-    def visa_17_digits(self):
-        number_of_tests = 10000
-        for i in range(number_of_tests):
-            num = random.randint(40000000000000000, 49999999999999999)
+    number_of_tests = 10000
+    for i in range(number_of_tests):
+        num = random.randint(40000000000000000, 49999999999999999)
 
-            if not credit_card_validator(str(num)):
-                print('Error {}')
+        if not credit_card_validator(str(num)):
+            print('Error {}')
 
-    def visa_16_digits(self):
-        number_of_tests = 10000
-        for i in range(number_of_tests):
-            num = random.randint(4000000000000000, 4999999999999999)
-            if not credit_card_validator(str(num)):
-                print('Error {}')
+    for i in range(number_of_tests):
+        num = random.randint(4000000000000000, 4999999999999999)
+        if not credit_card_validator(str(num)):
+            print('Error {}')
 
-    def visa_15_digits(self):
-        number_of_tests = 10000
-        for i in range(number_of_tests):
-            num = random.randint(400000000000000, 499999999999999)
-            if not credit_card_validator(str(num)):
-                print('Error {}')
+    for i in range(number_of_tests):
+        num = random.randint(400000000000000, 499999999999999)
+        if not credit_card_validator(str(num)):
+            print('Error {}')
 
 
 # Master Card generators
