05c8120102903913d6dc3695ae8b54eedf4f992e
Scott <hudsonsc@oregonstate.edu>
2020-07-13 15:51:17 -0600

    Fixed the Master Card tests to allow for outliers in a more accurate way. Also expanded Visa test to allow for outliers in general.

--- tests.py
+++ tests.py
@@ -13,24 +13,19 @@ class CreditCardTests(unittest.TestCase):
 
     # Visa Tests
 
-    # Visa prefix, more than 16 digits, check sum is random
+    # Visa prefix, less than 16 digits, check sum is random
     for i in range(number_of_tests):
-        num = random.randint(40000000000000000, 49999999999999999)
+        num = random.randint(390000000000000, 590000000000000)
         credit_card_validator(str(num))
 
     # Visa prefix, 16 digits, check sum is random
     for i in range(number_of_tests):
-        num = random.randint(4000000000000000, 4999999999999999)
+        num = random.randint(3900000000000000, 5900000000000000)
         credit_card_validator(str(num))
 
-    # Visa prefix, less than 16 digits, check sum is random
+    # Visa prefix, more than 16 digits, check sum is random
     for i in range(number_of_tests):
-        num = random.randint(400000000000000, 499999999999999)
-        credit_card_validator(str(num))
-
-    # Visa prefix, less than 16 digits, check sum is random
-    for i in range(number_of_tests):
-        num = random.randint(400000000000000, 499999999999999)
+        num = random.randint(39000000000000000, 59000000000000000)
         credit_card_validator(str(num))
 
     # Master Card tests
@@ -44,7 +39,7 @@ class CreditCardTests(unittest.TestCase):
     # Mastercard random prefix between 2221 and 2720 (allows for outliers)
     # less than 16 digits, check sum is random
     for i in range(number_of_tests):
-        num = random.randint(400000000000000, 499999999999999)
+        num = random.randint(222000000000000, 272999999999999)
         credit_card_validator(str(num))
 
     # Mastercard random prefix between 51 and 55 (allows for outliers)
@@ -56,22 +51,23 @@ class CreditCardTests(unittest.TestCase):
     # Mastercard random prefix between 2221 and 2720 (allows for outliers)
     # 16 digits, check sum is random
     for i in range(number_of_tests):
-        num = random.randint(4000000000000000, 4999999999999999)
+        num = random.randint(2220000000000000, 2729999999999999)
         credit_card_validator(str(num))
 
     # Mastercard random prefix between 51 and 55 (allows for outliers)
     # more than 16 digits, check sum is random
     for i in range(number_of_tests):
-        num = random.randint(500000000000000, 569999999999999)
+        num = random.randint(50000000000000000, 56999999999999999)
         credit_card_validator(str(num))
 
     # Mastercard random prefix between 2221 and 2720 (allows for outliers)
     # more than 16 digits, check sum is random
     for i in range(number_of_tests):
-        num = random.randint(40000000000000000, 49999999999999999)
+        num = random.randint(22200000000000000, 27299999999999999)
         credit_card_validator(str(num))
 
     # American Express tests
+
 
 
 if __name__ == '__main__':
